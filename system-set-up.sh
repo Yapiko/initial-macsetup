25ec3ef2a2dc577be2f0f26d5af6bd25e8a90ee0
Jesus Francisco Perez Mena <jesus.perez@yapiko.com>
2019-11-12 17:31:19 +0100

    Removing old functions dependency in the script

--- system-set-up.sh
+++ system-set-up.sh
@@ -12,14 +12,4 @@ echo "Running system setup ruby script"
 ruby system-set-up.rb
 echo "system setup ruby script finished"
 
-echo "Adding aliases and functions in your bash"
-count=$(grep -Fx "source ~/dev/src/systemsetup/functions.sh" ~/.zshrc | wc -l)
-
-if [ $count -eq 0 ];then
-	echo "\nsource ~/dev/src/systemsetup/functions.sh" >> ~/.zshrc
-	echo "source ~/.zshrc"
-else
-	echo "Functions are already imported"
-fi
-
 echo "Done!"
